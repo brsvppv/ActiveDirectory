6bcac504f6c8c570845ecc2d130c07003322b25c
Borislav G. Popov <65204241+brsvppv@users.noreply.github.com>
2021-09-12 14:26:27 +0300

    Add files via upload

---
+++ CreateADUser.ps1
@@ -0,0 +1,12 @@
+﻿$FirstName = (Read-Host First Name)
+$LastName = (Read-Host Last Name)
+$UserName = (Read-Host Windows Username)
+$Department = (Read-Host Department)
+$Description = (Read-Host  Account Description)
+
+$systemName = $firstName + " " + $lastName
+$logonName = $userName + "@" + $env:USERDNSDOMAIN 
+$userMail =  $userName + "@" + "$env:USERDNSDOMAIN"
+
+New-ADUser -Name $systemName -DisplayName $systemName -GivenName $FirstName -Surname $LastName -SamAccountName $UserName -UserPrincipalName $logonName -Department $Department -EmailAddress $userMail -Description $Description -AccountPassword(Read-Host  -AsSecureString "Account Password") -PassThru | Enable-ADAccount 
+
